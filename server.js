cc2ca1fe9e300d4bdedbfc62a17a820284965ec0
unknown <theankitbagchi@gmail.com>
2020-08-25 19:26:12 -0400

    UnifiedTopologie argument added to server

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
+  useUnifiedTopology: true,
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
